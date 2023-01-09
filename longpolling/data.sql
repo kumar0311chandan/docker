709a28bc0cd1450bf68f86861440dad8400e4569
kumar0311chandan <kumar0311chandan@gmail.com>
2023-01-09 19:05:47 +0530

    sql file and prop

---
+++ longpolling/data.sql
@@ -0,0 +1,12 @@
+insert into userjpabean values (1001, sysdate(), 'Chandan');
+insert into userjpabean values (1002, sysdate(), 'Rahul');
+insert into userjpabean values (1003, sysdate(), 'Tushar');
+
+insert into post values (11001, 'My First Post', 1001);
+insert into post values (11002, 'My Second Post', 1001);
+insert into post values (11003, 'New post by 1001', 1001);
+
+
+insert into messages values ('1e2c30da-532f-42e0-b789-56ea8a79f231', 'text1', '2023-01-08T01:21:31.951+00:00');
+insert into messages values ('2e2c30da-532f-42e0-b789-56ea8a79f232', 'text2', '2023-01-08T02:22:32.952+00:00');
+insert into messages values ('3e2c30da-532f-42e0-b789-56ea8a79f233', 'text3', '2023-01-08T03:23:33.953+00:00');
